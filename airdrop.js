25fc5726c18feca8f1f74a22d2c7673bf9efbcaa
volh <volh@bap.jp>
2024-06-21 09:20:58 +0700

    First move

--- airdrop.js
+++ airdrop.js
@@ -99,17 +99,17 @@ const main = async (wallet, toAddress, amountTransfer) => {
     }
 }
 
-const wallet = {
-    address: "0x8549fd85ef6509f212b306ae3d265049f6ddb2ac8b016e02b8c1882615f38a5a",
-    mnemonic: "post smile shy bubble history broccoli relief clump sketch brisk uphold coast",
-    path: "m/44'/637'/0'/0'/0'",
-    privateKey: "0x7051661af86713f98a9acdec8376540706036f3d300344845e055b2687a5e2a0",
-    publicKey: "0xea526ba1710343d953461ff68641f1b7df5f23b9042ffa2d2a798d3adb3f3d6c",
-};
+// const wallet = {
+//     address: "0x8549fd85ef6509f212b306ae3d265049f6ddb2ac8b016e02b8c1882615f38a5a",
+//     mnemonic: "post smile shy bubble history broccoli relief clump sketch brisk uphold coast",
+//     path: "m/44'/637'/0'/0'/0'",
+//     privateKey: "0x7051661af86713f98a9acdec8376540706036f3d300344845e055b2687a5e2a0",
+//     publicKey: "0xea526ba1710343d953461ff68641f1b7df5f23b9042ffa2d2a798d3adb3f3d6c",
+// };
 
-main(
-    wallet,
-    "0xd88e3752572b2356e3caf944ae3e627c03e1f8a451b2868c2f71e3be69773600",
-    "0x344222e045c7ac184f6ff54f6449a79f7ebae5f3a882bd1e31315a2dc916deb7",
-    1
-)
+// main(
+//     wallet,
+//     "0xd88e3752572b2356e3caf944ae3e627c03e1f8a451b2868c2f71e3be69773600",
+//     "0x344222e045c7ac184f6ff54f6449a79f7ebae5f3a882bd1e31315a2dc916deb7",
+//     1
+// )
